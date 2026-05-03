c438fdb7604b9df7020358afe85c6397120a64c8
Mateocas1 <castiglionemateo1@gmail.com>
2026-05-03 00:08:06 -0300

    fix: add convex generated stubs for build compatibility

---
+++ convex/_generated/server.d.ts
@@ -0,0 +1,32 @@
+/* eslint-disable */
+/**
+ * Generated utility types for server functions.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+import type {
+  FunctionReference,
+  FunctionReturnType,
+  FunctionArgs,
+  ApiFromModules,
+} from "convex/server";
+
+export declare const query: any;
+export declare const mutation: any;
+export declare const action: any;
+export declare const internalQuery: any;
+export declare const internalMutation: any;
+export declare const internalAction: any;
+export declare const httpAction: any;
+
+export type {
+  FunctionReference,
+  FunctionReturnType,
+  FunctionArgs,
+  ApiFromModules,
+};
+export { api } from "./api.js";
+export type { DataModel, Doc, Id, TableNames } from "./dataModel.js";
---
+++ convex/_generated/server.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+/**
+ * Generated utility types for server functions.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+import {
+  queryGeneric,
+  mutationGeneric,
+  actionGeneric,
+  internalQueryGeneric,
+  internalMutationGeneric,
+  internalActionGeneric,
+  httpActionGeneric,
+} from "convex/server";
+
+export const query = queryGeneric;
+export const mutation = mutationGeneric;
+export const action = actionGeneric;
+export const internalQuery = internalQueryGeneric;
+export const internalMutation = internalMutationGeneric;
+export const internalAction = internalActionGeneric;
+export const httpAction = httpActionGeneric;
